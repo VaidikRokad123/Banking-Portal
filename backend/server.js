a9ed3c0664cdd959aff09e3386afe303a20e1c4a
vaidik rokad <vaidik.rokad@gmail.com>
2026-02-16 18:34:24 +0530

    register/login

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,5 @@ const connectDB = require('./src/config/db.js')
 connectDB();
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
 });
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
-function connectDB() {
+async function connectDB() {
     try {
-        mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDB connected");
     } catch (error) {
         console.log(error);
         process.exit(1)
     }
+
 }
 
 module.exports = connectDB
